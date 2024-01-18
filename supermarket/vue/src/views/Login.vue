442dc37ced472d71dc9643339b104dd174a41eb2
Mnozzi <156660087+Mnozzi@users.noreply.github.com>
2024-01-18 21:31:08 +0800

    模板

---
+++ supermarket/vue/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import {reactive, ref} from "vue"
+import request from "@/utils/request";
+import {ElMessage} from "element-plus";
+import router from "@/router";
+
+
+</script>
+
+<style scoped>
+</style>
